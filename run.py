4f4e54aa723058c2cf10c7fce122583ec3184fd8
shufeng <454048489@qq.com>
2025-06-08 22:12:15 +0800

    初始化

--- run.py
+++ run.py
@@ -7,10 +7,10 @@ from recbole.utils import init_seed, init_logger
 
 if __name__ == '__main__':
 
-    # configurations initialization
-    config = Config(model='BPR', dataset='yelp')
+    # 配置初始化
+    config = Config(model='LightGCN', dataset='ml-100k', config_file_list=["recbole/properties/model/LightGCN.yaml"])
 
-    # init random seed
+    # 初始随机种子
     init_seed(config['seed'], config['reproducibility'])
 
     # logger initialization
@@ -20,23 +20,23 @@ if __name__ == '__main__':
     # write config info into log
     logger.info(config)
 
-    # dataset creating and filtering
+    # 数据集过滤
     dataset = create_dataset(config)
     logger.info(dataset)
 
-    # dataset splitting
+    # 数据集拆分
     train_data, valid_data, test_data = data_preparation(config, dataset)
 
-    # model loading and initialization
+    # 模型初始化
     model = BPR(config, train_data.dataset).to(config['device'])
     logger.info(model)
 
-    # trainer loading and initialization
+    # 训练器初始化
     trainer = Trainer(config, model)
 
-    # model training
+    # 模型训练
     best_valid_score, best_valid_result = trainer.fit(train_data, valid_data)
 
-    # model evaluation
+    # 模型评估
     test_result = trainer.evaluate(test_data)
     print(test_result)
